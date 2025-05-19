787938f6fd612fc93f11bfd6e4bbcea529225612
Vladimir Pruzhanskyi <vladimir.Pruzhanskyi@Student.HTW-Berlin.de>
2025-05-19 15:31:55 +0200

    Aufgabe 2 komplett

--- javasrc/de/htwberlin/dbtech/aufgaben/ue02/VersicherungJdbc.java
+++ javasrc/de/htwberlin/dbtech/aufgaben/ue02/VersicherungJdbc.java
@@ -5,7 +5,7 @@ package de.htwberlin.dbtech.aufgaben.ue02;
   @author Ingo Classen
  */
 
-import de.htwberlin.dbtech.exceptions.DataException;
+import de.htwberlin.dbtech.exceptions.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -13,6 +13,7 @@ import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.LinkedList;
@@ -40,52 +41,131 @@ public class VersicherungJdbc implements IVersicherungJdbc {
 
     @Override
     public List<String> kurzBezProdukte() {
-        List<String> produkteKurzBez = new LinkedList<String>();
-        String query = "SELECT kurzbez FROM produkt ORDER BY id ASC";
+
+        List<String> kurzBez = new LinkedList<String>();
+        String query = "SELECT kurzbez from produkt order by id asc";
         PreparedStatement ps = null;
         ResultSet rs = null;
-        try {
+
+        try{
             ps = useConnection().prepareStatement(query);
+
             rs = ps.executeQuery();
             while (rs.next()) {
-                produkteKurzBez.add(rs.getString("kurzbez"));
-            }
-        } catch (Exception e) {
-            L.error("Error executing query: " + query, e);
-        } finally {
-            try {
-                if (rs != null) rs.close();
-                if (ps != null) ps.close();
-            } catch (Exception e) {
-                L.error("Error closing resources", e);
+                kurzBez.add(rs.getString("kurzbez"));
             }
         }
+        catch (SQLException e) {
+            throw new DataException(e);
 
-        return produkteKurzBez;
+        }
+
+        return kurzBez;
     }
 
     @Override
     public Kunde findKundeById(Integer id) {
-        L.info("id: " + id);
-        L.info("ende");
-        return null;
+        String query = "SELECT id, name, geburtsdatum FROM kunde WHERE id = ?";
+        try (PreparedStatement ps = useConnection().prepareStatement(query)) {
+            ps.setInt(1, id);
+            ResultSet rs = ps.executeQuery();
+
+            if (rs.next()) {
+                Integer kid = rs.getInt("id");
+                String name = rs.getString("name");
+                LocalDate gebDatum = rs.getDate("geburtsdatum").toLocalDate();
+                return new Kunde(kid, name, gebDatum);
+            } else {
+                throw new KundeExistiertNichtException(id);
+            }
+        } catch (SQLException e) {
+            throw new DataException(e);
+        }
     }
 
     @Override
     public void createVertrag(Integer id, Integer produktId, Integer kundenId, LocalDate versicherungsbeginn) {
-        L.info("id: " + id);
-        L.info("produktId: " + produktId);
-        L.info("kundenId: " + kundenId);
-        L.info("versicherungsbeginn: " + versicherungsbeginn);
-        L.info("ende");
+        if (versicherungsbeginn.isBefore(LocalDate.now())) {
+            throw new DatumInVergangenheitException(versicherungsbeginn);
+        }
+
+        try {
+            // PrÃ¼fe: Vertrag existiert bereits?
+            PreparedStatement checkVertrag = useConnection().prepareStatement("SELECT COUNT(*) FROM vertrag WHERE id = ?");
+            checkVertrag.setInt(1, id);
+            ResultSet rs1 = checkVertrag.executeQuery();
+            if (rs1.next() && rs1.getInt(1) > 0) {
+                throw new VertragExistiertBereitsException(id);
+            }
+
+            // PrÃ¼fe: Produkt existiert?
+            PreparedStatement checkProdukt = useConnection().prepareStatement("SELECT COUNT(*) FROM produkt WHERE id = ?");
+            checkProdukt.setInt(1, produktId);
+            ResultSet rs2 = checkProdukt.executeQuery();
+            if (rs2.next() && rs2.getInt(1) == 0) {
+                throw new ProduktExistiertNichtException(produktId);
+            }
+
+            // PrÃ¼fe: Kunde existiert?
+            PreparedStatement checkKunde = useConnection().prepareStatement("SELECT COUNT(*) FROM kunde WHERE id = ?");
+            checkKunde.setInt(1, kundenId);
+            ResultSet rs3 = checkKunde.executeQuery();
+            if (rs3.next() && rs3.getInt(1) == 0) {
+                throw new KundeExistiertNichtException(kundenId);
+            }
+
+            // Versicherungsende berechnen
+            LocalDate ende = versicherungsbeginn.plusYears(1).minusDays(1);
+
+            // Insert Vertrag
+            PreparedStatement insertStmt = useConnection().prepareStatement(
+                    "INSERT INTO vertrag (id, Produkt_FK, Kunde_FK, versicherungsbeginn, versicherungsende) VALUES (?, ?, ?, ?, ?)");
+            insertStmt.setInt(1, id);
+            insertStmt.setInt(2, produktId);
+            insertStmt.setInt(3, kundenId);
+            insertStmt.setDate(4, java.sql.Date.valueOf(versicherungsbeginn));
+            insertStmt.setDate(5, java.sql.Date.valueOf(ende));
+            insertStmt.executeUpdate();
+
+        } catch (SQLException e) {
+            throw new DataException(e);
+        }
     }
 
     @Override
     public BigDecimal calcMonatsrate(Integer vertragsId) {
-        L.info("vertragsId: " + vertragsId);
+        try {
+            // Versicherungsbeginn holen
+            PreparedStatement beginnStmt = useConnection().prepareStatement(
+                    "SELECT versicherungsbeginn FROM vertrag WHERE id = ?");
+            beginnStmt.setInt(1, vertragsId);
+            ResultSet  rsBeginn = beginnStmt.executeQuery();
 
-        L.info("ende");
-        return null;
+            if (!rsBeginn.next()) {
+                throw new de.htwberlin.dbtech.exceptions.VertragExistiertNichtException(vertragsId);
+            }
+
+            LocalDate beginn = rsBeginn.getDate("versicherungsbeginn").toLocalDate();
+
+            // Summiere Deckungspreise basierend auf gÃ¼ltigem Zeitraum und Deckungsbetrag
+            PreparedStatement preisStmt = useConnection().prepareStatement(
+                    "SELECT dp.preis FROM deckungspreis dp " +
+                            "JOIN deckungsbetrag db ON dp.deckungsbetrag_fk = db.id " +
+                            "JOIN deckung d ON d.deckungsart_fk = db.deckungsart_fk AND d.deckungsbetrag = db.deckungsbetrag " +
+                            "WHERE d.vertrag_fk = ? AND ? BETWEEN dp.gueltig_von AND dp.gueltig_bis");
+            preisStmt.setInt(1, vertragsId);
+            preisStmt.setDate(2, java.sql.Date.valueOf(beginn));
+            ResultSet rsPreise = preisStmt.executeQuery();
+
+            BigDecimal summe = BigDecimal.ZERO;
+            while (rsPreise.next()) {
+                summe = summe.add(rsPreise.getBigDecimal("preis"));
+            }
+
+            return summe;
+        } catch (SQLException e) {
+            throw new DataException(e);
+        }
     }
 
 }
